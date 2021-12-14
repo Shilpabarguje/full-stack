700672c8e3a21991e87a3c5c7a0d701145ca2d87
= <shilpabarguje31@gmail.com>
2021-12-14 16:13:23 +0530

    shilpabarguje

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./Home";
  import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
  import Login from "./Forms/login/login";
 import Register from "./Forms/register/register";
-import Homepage from "./homepage/homepage"
+
 
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -22,14 +22,10 @@ const App = () => {
         <Route path="/about" component={About}></Route>
         <Route path="/contact" component={Contact}></Route>
         <Route path="/services" component={Service}></Route>
-        <Route path="/login" component={Login}></Route>
+        <Route path="/login" component={() => <Login setLoginUser={setLoginUser}></Login>}></Route>
         <Route path="/register" component={Register}></Route>
-        <Route exact path="/">
-            {
-              user && user._id ? <Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser}/>
-            }
-          </Route>
-          <Login setLoginUser={setLoginUser}/>
+        
+        
         
         <Route component={Error}></Route>
       </Switch>
@@ -40,4 +36,3 @@ const App = () => {
 };
 
 export default App;
-
--- src/navbar.js
+++ src/navbar.js
@@ -1,58 +1,70 @@
-import React,{useState} from "react";
-import {NavLink} from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./index.css";
 
-const Navbar =()=>{
- const[show,setShow]=useState()
-    return <>
-    <section className="navsection-bg">
-    <nav className="navbar navbar-expand-lg navbar-light ">
-  <div className="container">
-    <a className="navbar-brand" href="#">SaiInfo</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={()=>setShow(!show)}>
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className={`collapse navbar-collapse ${show ? "show":""}`}>
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-              <NavLink class="nav-link active"
-              aria-current="page" to="/">
-                Home
-              </NavLink>
-        </li>
-        <li class="nav-item">
-          <NavLink class="nav-link" to="/contact">
-          Services 
-          </NavLink>
-        </li>
-        <li class="nav-item">
-          <NavLink class="nav-link" to="/Services">
-          About
-          </NavLink>
-        </li>
-        <li class="nav-item">
-          <NavLink class="nav-link" to="/about">
-          Contact 
-          </NavLink>
-        </li>
-     </ul>
 
-        <form class ="d-flex">
-        <button class="btn btn-outline-success btn-style" type="submit">
-        <NavLink class="nav-link" to="/login">
-          Sign Up
-          </NavLink>
-          </button>
-        <button class="btn btn-outline-success btn-style" type="submit">
-        <NavLink class="nav-link" to="/register">
-          Login
-          </NavLink>
-        </button>
-        </form>
-     </div>
-  </div>
-</nav>
-</section>
- </>;
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <section className="navbar-bg">
+        <nav class="navbar navbar-expand-lg navbar-light ">
+          <div class="container">
+            <a class="navbar-brand" href="#">
+              GiantSoftTech
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              onClick={() => setShow(!show)}>
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class={`collapse navbar-collapse ${show ? "show" : ""}`}>
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <NavLink class="nav-link active" aria-current="page" to="/">
+                    Home
+                  </NavLink>
+                </li>
+                <li class="nav-item">
+                  <NavLink class="nav-link" to="/service">
+                    Services
+                  </NavLink>
+                </li>
+                <li class="nav-item">
+                  <NavLink class="nav-link" to="/about">
+                    About
+                  </NavLink>
+                </li>
+                <li class="nav-item">
+                  <NavLink class="nav-link" to="/contact">
+                    Contact
+                  </NavLink>
+                </li>
+                <li>
+                <NavLink to="/login" className="nav">
+                  Login
+                  </NavLink>
+                </li>
+                <li>
+                <NavLink to="/register" className="nav">
+                  Register
+                  </NavLink>
+                </li>
+                </ul>
+              
+            </div>
+          </div>
+        </nav>
+      </section>
+    </>
+  );
 };
 
 export default Navbar;
